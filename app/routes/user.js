0f965450853e01832fac954c3a4a872e20689bcd
Ruma303 <matteoh8@hotmail.it>
2024-05-04 13:26:39 +0200

    sposto passport.js

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const passport = require('../middlewares/passport');
+const passport = require('../config/passport');
 
 router
     .get('/login', (req, res) => {
